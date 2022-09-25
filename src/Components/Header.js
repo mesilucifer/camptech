24a92f4060a6cc692daa0f3ab5034e81c62be3ac
Ambika prasad porwal <57431782+mesilucifer@users.noreply.github.com>
2022-09-25 10:53:04 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,23 +7,7 @@ import {Link } from 'react-router-dom';
 function Header() {
     return (
         <div>
-        <Navbar className="border-bottom p-3 mt-4  nav" bg="white" expand="lg">
-        <Navbar.Brand className="mx-5 logo" style={{color:'green'}}>Camp<span style={{color:'darkgreen'}}>Tech</span></Navbar.Brand>
-        <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
-        <Navbar.Collapse id="navbar-toggle">
-          <Nav className="mx-4">
-          <Link className="nav-link me-4" to="/">Our Courses</Link>
-          <Link className="nav-link me-4" to="/quiz">Quiz</Link>
-          <Link className="nav-link me-4" to="/help">Help</Link>    
-          </Nav>
-        </Navbar.Collapse>
-        <div className="links">
-        <a href="https://campk12.com/"><i className="ri-facebook-fill"></i></a>
-        <a href="https://campk12.com/"><i className="ri-linkedin-fill"></i></a>
-        <a href="https://campk12.com/"><i className="ri-twitter-fill"></i></a>
-        <a href="https://campk12.com/"><i className="ri-github-fill"></i></a>
-        </div>
-    </Navbar>
+        Header
 
         </div>
     )
